2871f56480749613a98624a3c2b2f411de63a6bd
Alex Wellnitz <moin@wellnitz-alex.de>
2023-07-23 11:26:14 +0200

    Remove wrong implementation

--- src/configuration/configuration_loader.rs
+++ src/configuration/configuration_loader.rs
@@ -1,24 +1,14 @@
+use log::info;
 use std::{fs::File, io::BufReader};
 
 use crate::types::config::Config;
-
-// #[derive(Debug)]
-// pub struct ConfigurationLoader {}
-
-// impl ConfigurationLoader {
-//     pub fn load(&self, path: &str) -> Config {
-//         let file = File::open(path).unwrap();
-//         let reader = BufReader::new(file);
-//         let config: Config = serde_json::from_reader(reader).unwrap();
-        
-//         return config
-//     }
-// }
 
 pub fn load(path: &str) -> Config {
     let file = File::open(path).unwrap();
     let reader = BufReader::new(file);
     let config: Config = serde_json::from_reader(reader).unwrap();
-    
-    return config
+
+    info!("Load configuration successfully");
+
+    return config;
 }
